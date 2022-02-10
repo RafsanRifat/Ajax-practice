476a6f2bae728fbd903c3641c5810d5f68faef98
RafsanRifat <rafsan.rifat255@gmail.com>
2022-02-10 18:17:36 +0600

    Ajax practice added

---
+++ static/main.js
@@ -0,0 +1,24 @@
+// $(document).ready(function () {
+//
+//     $('#button').click(function () {
+//         $('#data').load('data.json');
+//     });
+//
+// });
+
+
+const getButton = document.getElementById('get-button');
+const sendButton = document.getElementById('send-button');
+
+const getData = function (){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1');
+    xhr.send();
+}
+
+const sendData = function (){
+
+}
+
+getButton.addEventListener('click', getData);
+sendButton.addEventListener('click', sendData);
